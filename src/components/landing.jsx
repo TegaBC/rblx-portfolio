b4c1a85f1081ba7234d256ff3e7c840d1c7456c4
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-14 19:16:25 +0100

    init basic landing

---
+++ src/components/landing.jsx
@@ -0,0 +1,14 @@
+
+export default function Landing() {
+    return <div className="h-[580px] bg-splash bg-cover bg-blend-darken text-text backdrop-brightness-50">
+       <div className="flex flex-col gap-6 items-center justify-center h-full">
+            <h1 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-8xl font-bold">TegaBC</h1>
+            <h2 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-xl">Gameplay Programmer</h2>
+            
+            <div className="flex gap-4 font-medium">
+                <button className="bg-main px-6 py-3 rounded-sm">Portfolio</button>
+                <button className="bg-main px-6 py-3 rounded-sm">Contact</button>
+            </div>
+       </div>
+    </div>
+}
