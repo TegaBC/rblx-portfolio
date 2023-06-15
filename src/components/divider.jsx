b0473351b0385f60e5d1413671d48c3afe419043
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 06:02:09 +0100

    flushed out home page basics and added divider

---
+++ src/components/divider.jsx
@@ -0,0 +1,6 @@
+
+export default function Divider() {
+    return <div className="w-full bg-main flex justify-center">
+        <div className="bg-white h-[2px] rounded-lg max-w-[500px] w-1/2"> </div>
+    </div>
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,5 @@
+import Contact from "../components/contact"
+import Divider from "../components/divider"
 import Landing from "../components/landing"
 import NavBar from "../components/navbar"
 
@@ -5,5 +7,78 @@ export default function HomePage() {
     return <>
         < NavBar/>
         < Landing/>
-    </>
+        <div className="flex flex-col items-center bg-main py-8 text-text">
+            <h1 className="text-7xl font-bold mb-4">ABOUT ME</h1>    
+            <h2 className="text-xl font-bold text-accent mb-4">Learn more about me.</h2>
+            <div className="flex flex-col gap-4 items-center">
+                <div className="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                    </svg>
+                    <p>London, UK</p>
+                </div>
+                <div className="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                    <path fillRule="evenodd" d="M3 6a3 3 0 013-3h12a3 3 0 013 3v12a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm14.25 6a.75.75 0 01-.22.53l-2.25 2.25a.75.75 0 11-1.06-1.06L15.44 12l-1.72-1.72a.75.75 0 111.06-1.06l2.25 2.25c.141.14.22.331.22.53zm-10.28-.53a.75.75 0 000 1.06l2.25 2.25a.75.75 0 101.06-1.06L8.56 12l1.72-1.72a.75.75 0 10-1.06-1.06l-2.25 2.25z" clipRule="evenodd" />
+                    </svg>
+                    <p>Gameplay Programmer</p>
+                </div>
+                <div className="flex gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                    <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clip-rule="evenodd" />
+                    </svg>
+
+                    <p>18 Years Old</p>
+                </div>
+            </div>
+        </div>
+        < Divider/>
+        <div className="flex flex-col items-center bg-main py-8 text-text">
+            <h1 className="text-7xl font-bold mb-4">SKILLS</h1>    
+            <h2 className="text-xl font-bold text-accent mb-4">My skillset.</h2>
+            <div className="flex flex-col gap-6 text-black">
+                <div className="flex gap-4">
+                    <div className="flex items-center justify-center w-16 h-16 rounded-full bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clipRule="evenodd" />
+                        </svg>
+
+                    </div>
+                    <div>
+                        <h1 className="text-accent font-medium">Programming</h1>
+                        <p className="text-text text-sm max-w-[35ch]">Proficient in Lua/Luau and fullstack web development</p>
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <div className="flex items-center justify-center w-16 h-16 rounded-full bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M12 6.75a5.25 5.25 0 016.775-5.025.75.75 0 01.313 1.248l-3.32 3.319c.063.475.276.934.641 1.299.365.365.824.578 1.3.64l3.318-3.319a.75.75 0 011.248.313 5.25 5.25 0 01-5.472 6.756c-1.018-.086-1.87.1-2.309.634L7.344 21.3A3.298 3.298 0 112.7 16.657l8.684-7.151c.533-.44.72-1.291.634-2.309A5.342 5.342 0 0112 6.75zM4.117 19.125a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75h-.008a.75.75 0 01-.75-.75v-.008z" clipRule="evenodd" />
+                        <path d="M10.076 8.64l-2.201-2.2V4.874a.75.75 0 00-.364-.643l-3.75-2.25a.75.75 0 00-.916.113l-.75.75a.75.75 0 00-.113.916l2.25 3.75a.75.75 0 00.643.364h1.564l2.062 2.062 1.575-1.297z" />
+                        <path fillRule="evenodd" d="M12.556 17.329l4.183 4.182a3.375 3.375 0 004.773-4.773l-3.306-3.305a6.803 6.803 0 01-1.53.043c-.394-.034-.682-.006-.867.042a.589.589 0 00-.167.063l-3.086 3.748zm3.414-1.36a.75.75 0 011.06 0l1.875 1.876a.75.75 0 11-1.06 1.06L15.97 17.03a.75.75 0 010-1.06z" clipRule="evenodd" />
+                        </svg>
+                    </div>
+                    <div>
+                        <h1 className="text-accent font-medium">Code Structure</h1>
+                        <p className="text-text text-sm max-w-[35ch]">High quality and well documented internal code structure to make life easy for others.</p>
+                    </div>
+                </div>
+                <div className="flex gap-4">
+                    <div className="flex items-center justify-center w-16 h-16 rounded-full bg-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path d="M21 6.375c0 2.692-4.03 4.875-9 4.875S3 9.067 3 6.375 7.03 1.5 12 1.5s9 2.183 9 4.875z" />
+                        <path d="M12 12.75c2.685 0 5.19-.586 7.078-1.609a8.283 8.283 0 001.897-1.384c.016.121.025.244.025.368C21 12.817 16.97 15 12 15s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.285 8.285 0 001.897 1.384C6.809 12.164 9.315 12.75 12 12.75z" />
+                        <path d="M12 16.5c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 15.914 9.315 16.5 12 16.5z" />
+                        <path d="M12 20.25c2.685 0 5.19-.586 7.078-1.609a8.282 8.282 0 001.897-1.384c.016.121.025.244.025.368 0 2.692-4.03 4.875-9 4.875s-9-2.183-9-4.875c0-.124.009-.247.025-.368a8.284 8.284 0 001.897 1.384C6.809 19.664 9.315 20.25 12 20.25z" />
+                        </svg>
+                    </div>
+                    <div>
+                        <h1 className="text-accent font-medium">Datastores</h1>
+                        <p className="text-text text-sm max-w-[35ch]">Complex datastore design and implementation that is solid in production.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        < Divider/>
+        < Contact/>
+    </> 
 }
