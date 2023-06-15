448146ee40ddaeb46fcf2b2e2cbbc8edd23b04b1
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 19:43:12 +0100

    removed unused classes

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,6 +1,6 @@
 
 export default function Landing() {
-    return <div className="h-[580px] bg-splash bg-cover bg-blend-darken text-text backdrop-brightness-50">
+    return <div className="h-[580px] bg-splash bg-cover text-text">
        <div className="flex flex-col gap-6 items-center justify-center h-full">
             <h1 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-8xl font-bold">Tega</h1>
             <h2 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-xl">Gameplay Programmer</h2>
