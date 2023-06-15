100402f317c3d4030075aebcec8d2397da2f7105
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 19:59:15 +0100

    init contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,19 @@
+import Socials from "../components/socials";
+import Footer from "../components/footer";
 import NavBar from "../components/navbar";
 
 export default function ContactPage() {
     return <>
         <NavBar />
+        <div className="h-[480px] bg-splash bg-cover text-text">
+            <div className="flex flex-col gap-6 items-center justify-center h-full">
+                <h1 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-8xl font-bold">CONTACT ME</h1>
+                <h2 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-xl">Ways to get in contact.</h2>
+            </div>
+        </div>
+        <div className="bg-main py-20">
+        <Socials />
+        </div>
+        <Footer />
     </>
 }
