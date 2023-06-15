1c4ee08cd1a96e9d5d59e02411f1c0ae614aadc9
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 06:01:54 +0100

    wording and padding change

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -2,12 +2,12 @@
 export default function Landing() {
     return <div className="h-[580px] bg-splash bg-cover bg-blend-darken text-text backdrop-brightness-50">
        <div className="flex flex-col gap-6 items-center justify-center h-full">
-            <h1 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-8xl font-bold">TegaBC</h1>
+            <h1 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-8xl font-bold">Tega</h1>
             <h2 className="[text-shadow:_0px_2px_26px_rgba(0_0_0_/_59%)] text-xl">Gameplay Programmer</h2>
             
             <div className="flex gap-4 font-medium">
-                <button className="bg-main px-6 py-3 rounded-sm">Portfolio</button>
-                <button className="bg-main px-6 py-3 rounded-sm">Contact</button>
+                <button className="bg-main px-7 py-3 rounded-md">Portfolio</button>
+                <button className="bg-main px-7 py-3 rounded-md">Contact</button>
             </div>
        </div>
     </div>
