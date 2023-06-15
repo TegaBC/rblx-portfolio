9061583232d8069d74ff359c4289872e44f580a4
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 16:37:26 +0100

    removed margins from nav

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@
 
 export default function NavBar () {
     return <div className="bg-main text-text h-24 flex items-center drop-shadow justify-around">
-        <h1 className="ml-16 font-semibold text-2xl">TegaBC</h1>
+        <h1 className="font-semibold text-2xl">TegaBC</h1>
         <div className="flex gap-4 font-semibold">
             <a href="" className="hover:text-accent transition-colors">Contact</a>
             <a href="" className="hover:text-accent transition-colors">Portfolio</a>
