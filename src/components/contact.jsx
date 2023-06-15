bbe51cc5f333825f00dccc6947dfcb7434f8abba
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-15 06:02:15 +0100

    started contact page

---
+++ src/components/contact.jsx
@@ -0,0 +1,7 @@
+
+export default function Contact() {
+    return <div className="flex flex-col items-center bg-main py-8 text-text">
+        <h1 className="text-7xl font-bold mb-4">CONTACT</h1>    
+        <h2 className="text-xl font-bold text-accent mb-4">Stay connected with social media.</h2>
+    </div>
+}
