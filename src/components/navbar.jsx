e569bc43d3da85859340051807b1fe76098bc97b
TegaBC <64938182+TegaBC@users.noreply.github.com>
2023-06-16 18:08:43 +0100

    adding image to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,13 @@ import { Link  } from "react-router-dom"
 
 export default function NavBar () {
     return <div className="bg-main text-text h-24 flex items-center drop-shadow justify-around">
-        <Link to="/" className="font-semibold text-2xl">TegaBC</Link>
+        <Link to="/" className="font-semibold text-2xl h-full">
+        <img className="h-full" src="/icon.png" alt="" />
+        </Link>
         <div className="flex gap-4 font-semibold">
-            <Link to="/contact" className="hover:text-accent transition-colors">Contact</Link>
             <Link to="/portfolio" className="hover:text-accent transition-colors">Portfolio</Link>
+            <Link to="/contact" className="hover:text-accent transition-colors">Contact</Link>
+
         </div>
     </div>
 } 
