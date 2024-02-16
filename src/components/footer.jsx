d8ec349caef0be4a87540904423ec96c1352881f
TegaBC <64938182+TegaBC@users.noreply.github.com>
2024-02-16 10:54:06 +0000

    changed footer year

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,6 @@
 
 export default function Footer() {
     return <div className="bg-[rgb(20,20,22)] flex center-items justify-center text-text py-5 font-light text-sm">
-        © TegaBC | 2023
+        © TegaBC | 2024
     </div>
 }
